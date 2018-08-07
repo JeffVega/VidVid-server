8d97ae470b1a0495b49c8b4270c9e627d27e2035
JeffVega <jeff.vega321@gmail.com>
2018-08-07 11:57:17 -0700

    message schema fixed

---
+++ models/message.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const mongoose = require('mongoose')
+
+const messageSchema = new mongoose.Schema({
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    message:{
+        type:String
+    },
+
+});
+
+
+messageSchema.set('toObject',{
+    transform: function(doc,ret){
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+    }
+})
+const Mess = mongoose.model('Message',messageSchema);
+module.exports = Mess
